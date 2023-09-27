e9dffceb8ef587761d6935293d48cf34e06f4d2e
Jerry Lane <lan16014@byui.edu>
2023-09-27 06:39:12 -0500

    Finished functions, added exe, update README

---
+++ Main.kt
@@ -0,0 +1,189 @@
+//simple Kotlin-based command-line Hangman game
+
+//function to create the default scaffold structure
+private fun createScaffold() : Array<String> {
+    val scaffold: Array<String> = arrayOf(
+        "",
+        "   ----------",
+        "   |        |",
+        "   |        ",
+        "   |       ",
+        "   |       ",
+        "   |",
+        "  --------------",
+        "  |            |",
+        ""
+    )
+    return scaffold
+}
+
+//function to make scaffold changes with hanged man
+private fun changeHangman(guesses: Int, hangmanArray: Array<String>) : Array<String> {
+
+    //change hangman's scaffold based on number of guesses used
+    when(guesses){
+        1 -> hangmanArray[5] += "\\"
+        2 -> hangmanArray[5] += "/ "
+        3 -> hangmanArray[4] += "\\"
+        4 -> hangmanArray[4] += "|"
+        5 -> hangmanArray[4] += "/"
+        6 -> hangmanArray[3] += "O"
+    }
+
+    //return the new hangman scaffold
+    return hangmanArray
+}
+
+//function to display guessed letters in the secret word
+private fun createHintString (guessedString: String, secretWord: String, userLetter: Char) : String {
+
+    //set blank string as default
+    var hintString : String  = ""
+
+    //if user letter is space then create new underlined string
+    if (userLetter == ' ') {
+        for (i: Int in 0 until secretWord.length) {
+            hintString += "_"
+        }
+
+    //otherwise, copy guessString to identifiedString and add userLetter if in secretWord
+    } else {
+        for (i: Int in 0 until secretWord.length) {
+            hintString += if (userLetter == secretWord[i]) {
+                userLetter
+            } else {
+                guessedString[i]
+            }
+        }
+    }
+
+    //return new hint string
+    return hintString
+}
+
+//function to determine if input char is in secret word
+private fun compareGuessToSecret(guess: Char, secret: String) : Boolean {
+
+    //return boolean of whether the char input is in the secret word
+    return guess in secret
+}
+
+//function to print the scaffold in the terminal
+private fun drawHangman(scaffold: Array<String>) {
+
+    //loop through scaffold array, print each part of it
+    for (part in scaffold) {
+        println(part)
+    }
+}
+
+//function to create a secret word if user didn't supply one
+private fun createSecret() : String {
+
+    //create the secret word array
+    val secretArray: Array<String> = arrayOf(
+        "skyscraper", "highway", "airliner", "country", "appliance",
+        "career", "projection", "library", "photographer", "countryman",
+        "warfare", "computations", "aquarium", "construction", "birthday"
+    )
+
+    //randomly select the secret word from the secret word array
+    val secretWord = secretArray[(0 .. secretArray.size - 1).random()]
+
+    //return secret word
+    return secretWord
+}
+
+//main function to drive the hangman program
+fun main(vararg args: String) {
+
+    //declare defaults
+    var userLetter : Char = '!'
+    var hintString = ""
+    var hangmanImage = createScaffold()
+    var guesses = 6
+    var control = true
+
+    //if user supplied secret word, use it, else create secret word
+    var secretWord = when (args.isNotEmpty()) {
+        true -> args[0]
+        else -> createSecret()
+    }
+
+    //create blank identified letters string
+    hintString = createHintString(
+        hintString, secretWord, userLetter = ' ')
+
+    //draw initial hangman scaffold
+    drawHangman(hangmanImage)
+
+    //game loop
+    while (control) {
+
+        //print hint string
+        println("Hint: $hintString")
+
+        //get letter from user
+        print("Guess a letter in the secret word (! to quit): ")
+        try {
+            userLetter = readln()[0].lowercaseChar()
+        } catch (e: Exception) {
+            println("\nInvalid input. Please type a letter.")
+        }
+
+        //if user letter = !, quit loop
+        if (userLetter == '!') {
+            control = false
+            println("Goodbye!\n")
+            continue
+        }
+
+        //if user letter was good, update identified string, draw hangman
+        if (compareGuessToSecret(userLetter, secretWord)) {
+            hintString = createHintString(
+                hintString, secretWord, userLetter
+            )
+            drawHangman(hangmanImage)
+
+        //else decrement guesses, advance hangman image, and draw hangman
+        } else {
+            hangmanImage = changeHangman(guesses, hangmanImage)
+            guesses--
+            drawHangman(hangmanImage)
+        }
+
+        //if identified string = secret word, print win message,
+        //ask user to play again, if no, break out of loop
+        if (hintString == secretWord || guesses == 0) {
+            if (hintString == secretWord) {
+                println("The word was: $secretWord. You win!")
+            } else {
+                println("You lose. The word was: $secretWord.")
+            }
+
+            //ask user if they want to play again
+            print("Play again (y/n)? ")
+            try {
+                userLetter = readln()[0].lowercaseChar()
+            } catch (e: Exception) {
+                println("\nInvalid input. Please type a letter.")
+            }
+
+            //if not y, set control to false, exit while loop
+            if (userLetter != 'y') {
+                control = false
+                println("Goodbye!\n")
+
+                //else reset game variables
+            } else {
+                println()
+                guesses = 6
+                secretWord = createSecret()
+                hintString = createHintString(
+                    hintString, secretWord, userLetter = ' ')
+                hangmanImage = createScaffold()
+                drawHangman(hangmanImage)
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Kotlin
-Kotlin-based command-line Hangman game 
+# Kotlin-Hangman
+
+## Overview
+
+Hangman is the typical letter-guessing program so well known throughout the world. This one is executed by command line and allows the user to include an argument which is then used as the secret word in the application. When it is run, a gallows is drawn, a hint string (for the secret word to be guessed) is displayed, and the user is asked to guess a letter. If the letter exists in the secret word, the hint string is modified to show the letter; if not, various parts of the hangman are drawn on the gallows. This continues until either the player guesses the secret word, a winning condition, or the hangman is drawn on the screen, and the game is lost. The player can quit by typing an exclamation point when asked for a letter, or once the player has won or lost by entering anything other than the letter 'y'.
+
+## Purpose
+
+This simple Hangman program is written in Kotlin. My purpose in writing this was to help me understand whether or not Kotlin supported a strictly console operation and give me some knowledge of the syntax of the Kotlin programming language. This exercise permitted me to learn Kotlin to the point I could apply that knowledge to create a small game application.
+
+## Software Demonstration
+
+[Software Demo Video](https://youtu.be/-Y0rQKXaEmA)
+
+The executable file which is included in this repository runs on the Windows 11 platform and is included here to demonstrate the software in realtime.
+
+## Development Environment
+
+### Development Platform
+
+Windows 11 Pro
+
+### Tools
+
+JetBrains IntelliJ IDE
+
+Kotlin programming language
+
+## Useful Websites
+
+The following websites were helpful as I learned Kotlin:
+
+- [Kotlin Keywords](https://kotlinlang.org/docs/keyword-reference.html#soft-keywords)
+- [Kotlin Tutorial](https://www.programiz.com/kotlin-programming)
+- [Kotline Video Tutorial](https://www.youtube.com/watch?v=F9UC9DY-vIU&t=121s)
+
+## Future Work
+
+There are many improvements which could be made to the game. Here are some I may implement at some point in the future:
+
+- Consolidate the various game variables into a single array, allowing for a function to set default values for the game to improve efficiency.
+- Create a Scaffold class to contain the graphic elements of the game with methods for drawing the gallows and altering the hangman image.
+- Create a Game class to hold the game logic in methods (I may or may not make the Scaffold class an embedded class here).
+- Use ANSI color codes to 'paint' the gallows and hangman image in color.
